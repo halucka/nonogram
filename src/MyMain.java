c6e5e7a4ba78fd475453e6f663a84a51a380830d
halucka <halucka@gmail.com>
2020-01-19 14:33:11 +0100

    eerste klasse, hello world

---
+++ src/MyMain.java
@@ -0,0 +1,9 @@
+/**
+ * @author Lucia Komendova
+ * @version 1.0 2020-01-17 21:29
+ */
+public class MyMain {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
